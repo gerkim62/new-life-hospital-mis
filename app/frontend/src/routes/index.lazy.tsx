19bab9a7e7e5bdbbfae72359ac31ed09ba7fcd3f
Gerison Kimathi <gerkim62@gmail.com>
2025-01-27 23:14:07 +0300

    added shadcn

--- app/frontend/src/routes/index.lazy.tsx
+++ app/frontend/src/routes/index.lazy.tsx
@@ -1,13 +1,17 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
 
-export const Route = createLazyFileRoute('/')({
+export const Route = createLazyFileRoute("/")({
   component: Index,
-})
+});
 
 function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <Button >
+        Example Button
+      </Button>
     </div>
-  )
+  );
 }
--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       "/api": {
