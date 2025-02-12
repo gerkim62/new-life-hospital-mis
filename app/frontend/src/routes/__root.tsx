73c326dfae04e566860e302cbaa5a59973199f58
Gerison Kimathi <gerkim62@gmail.com>
2025-02-12 10:19:22 +0300

    feat: add PWA support with vite-plugin-pwa and update layout for improved attribution display

--- app/frontend/src/routes/__root.tsx
+++ app/frontend/src/routes/__root.tsx
@@ -6,19 +6,18 @@ import "../index.css";
 export const Route = createRootRoute({
   component: () => (
     <div
-      className="min-h-screen bg-repeat bg-top bg-auto flex flex-col"
+      className="min-h-screen bg-repeat bg-top bg-auto flex flex-col "
       style={{ backgroundImage: "url('/bg.webp')" }}
     >
-      <div className="absolute inset-0 bg-[rgba(0,0,0,.2)] h-full"></div>
-
       <Navbar />
 
-      <div className="flex-1 flex flex-col items-center justify-center relative z-50">
-        <div className="w-full">
+      <div className="flex-1 flex flex-col items-center justify-center relative bg-[rgba(0,0,0,.5)]">
+        <div className="w-full ">
           <Outlet />
+
+          <Attribution />
         </div>
       </div>
-      <Attribution />
     </div>
   ),
 });
--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -3,10 +3,43 @@ import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), tailwindcss()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    tailwindcss(),
+    VitePWA({
+      injectRegister: "auto",
+      registerType: "autoUpdate",
+      manifest: {
+        name: "My Awesome PWA",
+        short_name: "MyPWA",
+        description: "An installable PWA built with Vite",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone", // Makes it installable
+        icons: [
+          {
+            src: "/pwa-icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
